a0e3e7095b29bdf504e80b386f1b911ad120b603
leon-carvalho <leonardoscc15@gmail.com>
2021-08-25 22:24:21 -0300

    Update Main component content

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,8 +1,22 @@
+import * as S from './styles'
+
 const Main = () => {
   return (
-    <main>
-      <h1>NextJS Boilerplate</h1>
-    </main>
+    <S.Wrapper>
+      <S.Logo
+        src="/img/logo.svg"
+        alt="Imagem de um átomo e React Avançado escrito ao lado"
+      />
+      <S.Title>NextJS Boilerplate</S.Title>
+      <S.Description>
+        TypeScript, ReactJS, Styled Components, Jest, Testing Library
+      </S.Description>
+
+      <S.Illustration
+        src="/img/hero-illustration.svg"
+        alt="Um desenvolvedor de frente para uma tela com código"
+      />
+    </S.Wrapper>
   )
 }
 
